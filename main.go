4a497759c885370dd1d9d294a668432063ba9a23
unknown <yfarhod75@gmail.com>
2021-03-24 17:37:34 +0100

    scheme commit

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(findUniquePaths(4, 4, makeBoard(5, 5), makeMem(5, 5)))
+}
+
+func findUniquePaths(row int, col int, board [][]string, mem [][]int) int {
+	return 1
+}
+
+func makeBoard(rows int, cols int) [][]string {
+	return 1
+}
+
+func makeMem(rows int, cols int) [][]int {
+	return 1
+}
